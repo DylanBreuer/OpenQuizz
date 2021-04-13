97f52d21c4f29b18ac5e5b87046f8c85b8f7f3ed
« Dylan <breuer.dylan.pro@gmail.com>
2021-04-13 13:03:33 +0200

    Enrichment of the Game model

--- OpenQuizz/OpenQuizz/Model/Game.swift
+++ OpenQuizz/OpenQuizz/Model/Game.swift
@@ -8,26 +8,38 @@
 import Foundation
 
 class Game {
-    enum State { case ongoing, over }
-    
     var score = 0
 
     private var questions = [Question]()
     private var currentIndex = 0
+
+    var state: State = .ongoing
+
+    enum State {
+        case ongoing, over
+    }
+
     var currentQuestion: Question {
         return questions[currentIndex]
     }
-    
-    var state : State = .ongoing
 
     func refresh() {
         score = 0
         currentIndex = 0
-        state = .ongoing
+        state = .over
+        
+        QuestionManager.shared.get { (questions) in
+            self.questions = questions
+            self.state = .ongoing
+            
+            let name = Notification.Name(rawValue: "QuestionsLoaded")
+            let notification = Notification(name: name)
+            NotificationCenter.default.post(notification)
+        }
     }
 
     func answerCurrentQuestion(with answer: Bool) {
-        if currentQuestion.isCorrect == answer {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
             score += 1
         }
         goToNextQuestion()
@@ -44,4 +56,11 @@ class Game {
     private func finishGame() {
         state = .over
     }
+    
+    /*
+    private func receiveQuestions(_ questions: [Question]) {
+        self.questions = questions
+        state = .ongoing
+    }
+ */
 }
